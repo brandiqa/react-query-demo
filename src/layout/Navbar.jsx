3a4343fc77979ee9b0ac890d2a5b78dc53b93d7e
Michael Wanyoike <micwanyoike@gmail.com>
2021-04-20 14:30:05 +0300

    Fixed missing logo andd favicon

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,9 +5,9 @@ import './navbar.css'
 
 function Navbar() {
   return (
-    <nav className="container flex mx-auto p-4 justify-between lg:w-screen-lg">
+    <nav className="container flex justify-between p-4 mx-auto lg:w-screen-lg">
       <div>
-        <img src="./logo.svg" alt="React Query" width="200" />
+        <img src="/logo.svg" alt="React Query" width="200" />
       </div>
       <div className="flex">
         <NavLink to="/" exact>
