a8eb57bb087c06476f12303d6b7ac425f46bec5d
Michael Wanyoike <micwanyoike@gmail.com>
2021-04-21 21:10:50 +0300

    Implemented Create User api logic

--- src/views/CreateUser.jsx
+++ src/views/CreateUser.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import axios from 'axios'
+import { useMutation } from 'react-query'
 
 import UserForm from '../components/UserForm'
+import { Redirect } from 'react-router-dom'
 
 function CreateUser() {
+  const mutation = useMutation((newUser) =>
+    axios.post('http://localhost:3004/users', newUser)
+  )
+  const { isLoading, isError, error, isSuccess } = mutation
+
   const onSubmit = async (data) => {
-    console.log(data)
+    mutation.mutate(data)
   }
 
   return (
     <div>
       <h2>New User</h2>
+
+      {isError && <div>An error occurred: {error.message}</div>}
+
+      {isLoading && <div>Loading...</div>}
+
+      {isSuccess && <Redirect to="/" />}
+
       <UserForm submitText="Create" submitAction={onSubmit} />
     </div>
   )
